5c81997cbab726ee7ac61cabcfc1448b6e23b5c5
Alberto Cavadia <osoflash_2@hotmail.com>
2015-09-10 19:02:18 -0430

    cambios solicitados por tibisay y eira, realizados

--- js/custom.js
+++ js/custom.js
@@ -129,15 +129,6 @@ Drop Down Menu Fade Effect
             }
         });
 
-        // Helper function to Fill and Center the HTML5 Video
-        jQuery('video,object').maximage('maxcover');
-
-
-
-
-
-        // To show it is dynamic html text
-
     });
 
 
@@ -371,41 +362,6 @@ jQuery(document).ready(function () {
         });
     });
 
-
-/*----------------------------------------------------*/
-/*  PrettyPhoto
-/*----------------------------------------------------*/
-
-    jQuery(function(){
-        jQuery("a[data-gal^='prettyPhoto']").prettyPhoto({
-              opacity: 0.5,
-              social_tools: "",
-              deeplinking: false
-        });
-
-        jQuery('a[data-rel^="prettyPhoto"]').prettyPhoto();
-    });
-
-
-
-    jQuery("#horizontal-tabs").tytabs({
-        tabinit: "1",
-        fadespeed: "fast"
-    });
-
-
-
-
-
-/*===============================================*/
-/*  Video Script
-/*===============================================*/
-
-    jQuery(function(){
-        jQuery(".player").mb_YTPlayer();
-    });
-
-
 /*----------------------------------------------------*/
 /*  Carousel Section
 /*----------------------------------------------------*/
@@ -416,104 +372,6 @@ jQuery(document).ready(function () {
     jQuery('.bxslider').carousel({interval: 5000, pause: "hover"});
 
 });
-
-
-
-
-/*----------------------------------------------------*/
-/*  Portfolio Isotope
-/*----------------------------------------------------*/
-
-
-
-
-    jQuery(document).ready(function(){
-
-        // Portfolio
-        (function($) {
-            "use strict";
-            var $container = $('#portfolio-wrap'),
-                $items = $container.find('.portfolio-item'),
-                portfolioLayout = 'fitRows';
-
-                if( $container.hasClass('portfolio-centered') ) {
-                    portfolioLayout = 'masonry';
-                }
-
-                $container.isotope({
-                    filter: '*',
-                    animationEngine: 'best-available',
-                    layoutMode: portfolioLayout,
-                    animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                },
-                masonry: {
-                }
-                }, refreshWaypoints());
-
-                function refreshWaypoints() {
-                    setTimeout(function() {
-                    }, 1000);
-                }
-
-                $('#filters a').on('click', function() {
-                        var selector = $(this).attr('data-filter');
-                        $container.isotope({ filter: selector }, refreshWaypoints());
-                        $('#filters a').removeClass('active');
-                        $(this).addClass('active');
-                        return false;
-                });
-
-                function getColumnNumber() {
-                    var winWidth = $(window).width(),
-                    columnNumber = 1;
-
-                    if (winWidth > 1200) {
-                        columnNumber = 4;
-                    } else if (winWidth > 950) {
-                        columnNumber = 4;
-                    } else if (winWidth > 600) {
-                        columnNumber = 3;
-                    } else if (winWidth > 400) {
-                        columnNumber = 2;
-                    } else if (winWidth > 250) {
-                        columnNumber = 1;
-                    }
-                        return columnNumber;
-                    }
-
-                    function setColumns() {
-                        var winWidth = $(window).width(),
-                        columnNumber = getColumnNumber(),
-                        itemWidth = Math.floor(winWidth / columnNumber);
-
-                        $container.find('.portfolio-item').each(function() {
-                            $(this).css( {
-                            width : itemWidth + 'px'
-                        });
-                    });
-                }
-
-                function setPortfolio() {
-                    setColumns();
-                    $container.isotope('reLayout');
-                }
-
-                $container.imagesLoaded(function () {
-                    setPortfolio();
-                });
-
-                $(window).on('resize', function () {
-                setPortfolio();
-            });
-        })(jQuery);
-
-
-
-
-    });
 
 
 
@@ -595,43 +453,6 @@ jQuery(document).ready(function(){
 
 
 });
-
-/*----------------------------------------------------*/
-/*  Contact Form Section
-/*----------------------------------------------------*/
-    $("#contact").submit(function (e) {
-        e.preventDefault();
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var subject = $("#subject").val();
-        var text = $("#text").val();
-        var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&text=' + text;
-
-
-        function isValidEmail(emailAddress) {
-            var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-            return pattern.test(emailAddress);
-        };
-
-        if (isValidEmail(email) && (text.length > 100) && (name.length > 1)) {
-            $.ajax({
-                type: "POST",
-                url: "ajax/process.php",
-                data: dataString,
-                success: function () {
-                    $('.success').fadeIn(1000).delay(3000).fadeOut(1000);
-                    $('#contact')[0].reset();
-                }
-            });
-        } else {
-            $('.error').fadeIn(1000).delay(5000).fadeOut(1000);
-
-        }
-
-        return false;
-    });
-
-
 
  /* ==============================================
 Firefox anchor fix
