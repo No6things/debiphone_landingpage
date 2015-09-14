ace52e920fa2cd9b3bee39d672ed0b3aa5fc7c87
Alberto Cavadia <osoflash_2@hotmail.com>
2015-09-14 14:25:10 -0430

    credenciales de debiphone

--- js/custom.js
+++ js/custom.js
@@ -168,9 +168,9 @@ Drop Down Menu Fade Effect
 
         var text = "A new user subscribed to Debiphone's newsletter is trying to contact you: \n"  + name+ "\n "+message;
 
-        var dataString ='api_user=acavadia'+'&api_key=InWork123'+ '&to=acavadia@inworknet.com' + '&toname=Debiphone' + '&subject=' + subject + '&from='+email + '&text=' + text ;
+        var dataString ='api_user=debiphone'+'&api_key=deb1t4ndo'+ '&to=debiphone@support.com' + '&toname=Debiphone' + '&subject=' + subject + '&from='+email + '&text=' + text ;
 
-        var dataString2 ='api_user=acavadia'+'&api_key=InWork123'+'&list=Debiphone' + '&data=' + JSON.stringify(subscriberData);
+        var dataString2 ='api_user=debiphone'+'&api_key=deb14ndo'+'&list=Debiphone' + '&data=' + JSON.stringify(subscriberData);
 
         function isValidEmail(email) {
           var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
@@ -257,9 +257,9 @@ $("#unsubscribe-buttom")
 
     var text = "An user has unsubscribed to Debiphone's newsletter:\n"  + email+ "\n "+message;
 
-    var dataString ='api_user=acavadia'+'&api_key=InWork123'+'&list=Debiphone' + '&email=' + email;
+    var dataString ='api_user=debbiphone'+'&api_key=deb1t4ndo'+'&list=Debiphone' + '&email=' + email;
 
-    var dataString2 ='api_user=acavadia'+'&api_key=InWork123'+ '&to=acavadia@inworknet.com' + '&toname=Debiphone' + '&subject=' + subject + '&from=debiphone@support.com' + '&text=' + text ;
+    var dataString2 ='api_user=debiphone'+'&api_key=deb1t4ndo'+ '&to=debiphone@support.com' + '&toname=Debiphone' + '&subject=' + subject + '&from=debiphone@support.com' + '&text=' + text ;
 
 
     function isValidEmail(email) {
